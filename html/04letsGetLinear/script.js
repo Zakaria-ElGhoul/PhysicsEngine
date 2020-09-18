bd2fcc9df4c96dad27b85f01b4aec27d61d34d84
Zakaria-ElGhoul <30044@ma-web.nl>
2020-09-18 18:43:02 +0200

    alles gefixt qua merge errors

--- html/04letsGetLinear/script.js
+++ html/04letsGetLinear/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -29,35 +28,3 @@ function animate()
 setInterval(animate, 10);
 
 
-=======
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-canvas.width = width;
-canvas.height = height;
-
-let myLine = new LinearFunction(-0.5,500);
-
-let A = new Point(100,100,20,"blue",true);
-let B = new Point(500,200,20,"green",true);   
-
-
-function animate()
-{
-  context.clearRect(0,0,width,height)
-
-  A.draw(context);
-  B.draw(context);
-  myLine.slope = (B.y - A.y)/(B.x -A.x);
-  myLine.intercept = B.y - myLine.slope * B.x;
-
-  myLine.draw(context);
-}
-
-setInterval(animate, 10);
-
-
->>>>>>> 2b64b1abfda69878bff84737786c0c017c3a1bcd
--- html/05PerpendicularLine/script.js
+++ html/05PerpendicularLine/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -41,47 +40,3 @@ function animate()
 setInterval(animate, 10);
 
 
-=======
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-const width = window.innerWidth;
-const height = window.innerHeight;
-
-canvas.width = width;
-canvas.height = height;
-
-
-let A,B,C,l,m;
-
-A = new Point(200,200,20,"red",true);
-B = new Point(600,300,20,"blue",true);
-C = new Point(200,500,20,"purple",true);
-l = new LinearFunction(-0.5,0);
-m = new LinearFunction(1,1);
-
-
-
-function animate()
-{
-  context.clearRect(0,0,width,height)
-  A.draw(context);
-  B.draw(context);
-  C.draw(context);
-
-  l.slope = (B.y - A.y) / (B.x - A.x)
-  l.intercept = B.y - B.x * l.slope;
-  l.draw(context);
-
-  m.slope = -1 / l.slope;
-  m.intercept = C.y - C.x * m.slope;
-
-  m.draw(context);
-
-  
-}
-
-setInterval(animate, 10);
-
-
->>>>>>> 2b64b1abfda69878bff84737786c0c017c3a1bcd
